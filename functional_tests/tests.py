5db7f8baad2cd9fef890b3d7056665c21a442807
Nilaksh Agarwal <agarwal.nilaksh@gmail.com>
2022-05-20 22:20:46 +0530

    Chap7.4

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -68,13 +68,6 @@ class NewVisitorTest(LiveServerTestCase):
 		self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
 		self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-		# Edith wonders whether the site will remember her list. Then she sees
-		# that the site has generated a unique URL for her -- there is some
-		# explanatory text to that effect.
-		self.fail('Finish the test!')
-
-		# She visits that URL - her to-do list is still there.
-
 		# Satisfied, she goes back to sleep
 
 	def test_multiple_users_can_start_lists_at_different_urls(self):
@@ -120,5 +113,5 @@ class NewVisitorTest(LiveServerTestCase):
 		page_text = self.browser.find_element_by_tag_name('body').text
 		self.assertNotIn('Buy peacock feathers', page_text)
 		self.assertIn('Buy milk', page_text)
-		
+
 		# Satisfied, they both go back to sleep
