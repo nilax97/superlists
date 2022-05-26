4816c2a842e6cb480061c032496a3d3d0e3ab010
Nilaksh Agarwal <agarwal.nilaksh@gmail.com>
2022-05-26 22:47:41 +0530

    updates

--- superlists/settings.py
+++ superlists/settings.py
@@ -115,6 +115,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+CSRF_TRUSTED_ORIGINS=['http://django-staging.nilaksh.me',]
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
