5d9c6c75a44bf44bf9956d06a2b9427a91f615b2
Nilaksh Agarwal <agarwal.nilaksh@gmail.com>
2022-06-02 23:00:57 +0530

    updates

--- superlists/settings.py
+++ superlists/settings.py
@@ -30,6 +30,23 @@ TEMPLATE_DEBUG = DEBUG
 
 ALLOWED_HOSTS = []
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+        },
+    },
+    'root': {'level': 'INFO'},
+}
+
 
 # Application definition
 
